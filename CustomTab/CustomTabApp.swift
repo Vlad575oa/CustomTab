d1a295c6dd291aec215f62357bb4fb53070a7e9b
Vlad <vlad14196@gmail.com>
2022-12-15 10:02:44 +0300

    Initial Commit

---
+++ CustomTab/CustomTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabApp.swift
+//  CustomTab
+//
+//  Created by user on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
